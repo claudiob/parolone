a1a94d074fe61ea305382ef2a9a3af610f28c333
claudiob <claudiob@gmail.com>
2012-05-31 19:51:52 -0700

    Empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parolaio::Application.config.secret_token = '50ed57231d99f9fa5112773a82371a0c89c9fcd5d78c03401d34366830784ccb437bfb21ce7a4439c9b416efad7273026cc5ae16ee7c129ce43ac65e1cd6710f'
